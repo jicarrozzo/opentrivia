49b542eaafa521aaee6959a8c4749be93d9c0f39
Juan Ignacio Carrozzo <jicar@Lobo-Notebook.sisorg.com.ar>
2018-11-11 17:46:28 -0300

    Categories and questions methods

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { OpenTriviaService } from '../services/open-trivia.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss']
 })
 export class HomePage {
+	questions: any;
+	categories: any;
 
+	constructor(private loadingCtrl: LoadingController, private opentriviaService: OpenTriviaService) {}
+
+	ngOnInit() {
+		// this.
+		this.getQuestions();
+	}
+
+	async getCategories() {
+		await this.opentriviaService.getCategories().subscribe(
+			res => {
+				console.log(res);
+				this.categories = res;
+			},
+			err => {
+				console.log(err);
+			}
+		);
+	}
+
+	async getQuestions() {
+		const loading = await this.loadingCtrl.create({
+			message: 'Getting questions',
+			duration: 2000
+		});
+		await loading.present();
+
+		await this.opentriviaService.getQuestions().subscribe(
+			res => {
+				console.log(res);
+				this.questions = res;
+				loading.dismiss();
+			},
+			err => {
+				console.log(err);
+				loading.dismiss();
+			}
+		);
+	}
 }
--- src/app/services/open-trivia.service.ts
+++ src/app/services/open-trivia.service.ts
@@ -1,9 +1,55 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap, map } from 'rxjs/operators';
+
+const url_questions: string = 'https://opentdb.com/api.php?amount=10';
+const url_categories: string = 'https://opentdb.com/api_category.php';
+
+const headers = {
+	headers: new HttpHeaders({
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+		'Content-Type': 'application/json'
+	})
+};
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class OpenTriviaService {
+	constructor(public http: HttpClient) {}
 
-  constructor() { }
+	//#region private handlers
+	private handleError(error: HttpErrorResponse) {
+		if (error.error instanceof ErrorEvent) {
+			// A client-side or network error occurred. Handle it accordingly.
+			console.error('An error occurred:', error.error.message);
+		} else {
+			// The backend returned an unsuccessful response code.
+			// The response body may contain clues as to what went wrong,
+			console.error(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
+		}
+		// return an observable with a user-facing error message
+		return throwError('Something bad happened; please try again later.');
+	}
+
+	private extractData(res: Response) {
+		let body = res;
+		return body || {};
+	}
+	//#endregion
+
+	getCategories(): Observable<any> {
+		return this.http.get(url_categories, headers).pipe(
+			map(this.extractData),
+			catchError(this.handleError)
+		);
+	}
+	getQuestions(): Observable<any> {
+		return this.http.get(url_questions, headers).pipe(
+			map(this.extractData),
+			catchError(this.handleError)
+		);
+	}
 }
